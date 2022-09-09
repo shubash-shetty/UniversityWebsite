8716091b6c04d93b368614de2ad7cd6849df1e5c
Shubash Shetty <103719734+shubash-shetty@users.noreply.github.com>
2022-09-09 14:20:19 +0530

    Add files via upload

---
+++ php/footerbottom.php
@@ -0,0 +1,7 @@
+	
+</div>
+<footer class="container">
+	<h3 style="text-align:center;background:#2A2B3C;margin:0;padding:15px;color:#fff;font-weight:normal">&copy;Developed by Shubash and Sukanya- <?php echo date("Y");?> </h3>
+</footer>
+
+</html>
